34197448d9ccb605ba98f045ca50ecca5f8d95ef
Tatsuya Ogawa <t.ogawa93@gmail.com>
2023-06-03 18:56:34 +0900

    Initial commit

---
+++ ckrx65n_threadx/src/ckrx65n/demo_printf.h
@@ -0,0 +1,11 @@
+
+#ifndef DEMO_PRINTF_H
+#define DEMO_PRINTF_H
+
+#define LOG_TERMINAL_INIT demo_printf_init
+#define LOG_TERMINAL demo_printf
+
+void demo_printf_init(void);
+void demo_printf(char *format, ...);
+
+#endif
